ae5a2b99c2e5031fa8d9265f0d0782fbd571d306
Salma2007 <salmakhaled7002@gmail.com>
2026-08-12 21:44:40 +0300

    Add input validation

--- Models/Process.cs
+++ Models/Process.cs
@@ -9,7 +9,19 @@ namespace OS_Project.Models
     public class Process
     {
         public int Id { get; set; }
+
         public int ArrivalTime { get; set; }
+
         public int BurstTime { get; set; }
+
+        public int Priority { get; set; }
+
+        public int WaitingTime { get; set; }
+
+        public int TurnaroundTime { get; set; }
+
+        public int CompletionTime { get; set; }
+
+        public int RemainingTime { get; set; }
     }
 }
---
+++ Validation/InputValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Project.Validation
+{
+    public static class InputValidator
+    {
+        public static bool IsValidProcessData(
+            string arrivalTimeText,
+            string burstTimeText,
+            string priorityText,
+            out int arrivalTime,
+            out int burstTime,
+            out int priority)
+        {
+            arrivalTime = 0;
+            burstTime = 0;
+            priority = 0;
+
+            if (!int.TryParse(arrivalTimeText, out arrivalTime))
+                return false;
+
+            if (!int.TryParse(burstTimeText, out burstTime))
+                return false;
+
+            if (!int.TryParse(priorityText, out priority))
+                return false;
+
+            if (arrivalTime < 0)
+                return false;
+
+            if (burstTime <= 0)
+                return false;
+
+            if (priority < 0)
+                return false;
+
+            return true;
+        }
+    }
+}
