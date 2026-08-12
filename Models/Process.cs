de7de10a9d033b505b2614fbc4bd7749936dc847
Salma2007 <salmakhaled7002@gmail.com>
2026-08-12 18:43:58 +0300

    Added Process Class

---
+++ Models/Process.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Project.Models
+{
+    public class Process
+    {
+        public int Id { get; set; }
+        public int ArrivalTime { get; set; }
+        public int BurstTime { get; set; }
+    }
+}
