0496e8dba44f2eb1d4e095cbf0e763789e86a07c
RagyFady <ragy.fady@msa.edu.eg>
2026-08-17 07:00:33 +0300

    Ragy

--- Forms/SJF(Non-Pre)Form.cs
+++ Forms/SJF(Non-Pre)Form.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using OS_Project.Models;
+using OS_Project.Algorithms;
 
 namespace OS_Project.Forms
 {
